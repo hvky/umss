e6955d1d11ddddfb52666bbe61aa1cf3b9d4509d
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-04-10 21:46:51 -0400

    cdn defined

--- 01-interfaz/src/main.js
+++ 01-interfaz/src/main.js
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap-icons/font/bootstrap-icons.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import './style.css'
 import { getPointsCircle, getPointsTriangle } from './points'
 
