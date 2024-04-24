1a7be45df5806def5333a618d42029c79222c52a
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-04-24 11:05:55 -0400

    X

--- querys.sql
+++ querys.sql
@@ -33,99 +33,99 @@ GROUP BY
 
 
 -- *****************************************************************
-SELECT 
-  ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS Puesto,
-  c.titulo AS Titulo_Pelicula,
-  COUNT(*) AS Cantidad_Visualizaciones
-FROM 
-  VISUALIZACION v
-JOIN 
-  CONTENIDO c ON v.id_contenido = c.id
-JOIN 
-  CUENTA cu ON v.id = cu.id_persona
-JOIN 
-  PERSONA p ON cu.id_persona = p.id
-JOIN 
-  CIUDAD ci ON p.id_ciudad_residencia = ci.id
-JOIN 
-  PAIS pa ON ci.id_pais = pa.id
-WHERE 
-  pa.nombre = 'Bolivia'
-  AND MONTH(v.fecha) = 2
-  AND YEAR(v.fecha) = 2024
-GROUP BY 
-  c.titulo
-ORDER BY 
-  Cantidad_Visualizaciones DESC
-LIMIT 5;
+-- SELECT 
+--   ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS Puesto,
+--   c.titulo AS Titulo_Pelicula,
+--   COUNT(*) AS Cantidad_Visualizaciones
+-- FROM 
+--   VISUALIZACION v
+-- JOIN 
+--   CONTENIDO c ON v.id_contenido = c.id
+-- JOIN 
+--   CUENTA cu ON v.id = cu.id_persona
+-- JOIN 
+--   PERSONA p ON cu.id_persona = p.id
+-- JOIN 
+--   CIUDAD ci ON p.id_ciudad_residencia = ci.id
+-- JOIN 
+--   PAIS pa ON ci.id_pais = pa.id
+-- WHERE 
+--   pa.nombre = 'Bolivia'
+--   AND MONTH(v.fecha) = 2
+--   AND YEAR(v.fecha) = 2024
+-- GROUP BY 
+--   c.titulo
+-- ORDER BY 
+--   Cantidad_Visualizaciones DESC
+-- LIMIT 5;
 
 
--- *****************************************************************
-SELECT
-  MIN(t.fecha_estreno) AS Fecha_Primer_Capitulo,
-  COUNT(DISTINCT t.id) AS Total_Temporadas,
-  SUM(t.cantidad_capitulos) AS Total_Capitulos,
-  COUNT(DISTINCT re.id_empresa) AS Empresas_Produccion,
-  MAX(c.fecha_estreno) AS Fecha_Ultimo_Capitulo
-FROM 
-  TEMPORADA t
-JOIN 
-  CAPITULO c ON t.id = c.id_temporada
-JOIN 
-  R_CONTENIDO_EMPRESA re ON t.id_serie = re.id_contenido
-JOIN 
-  CONTENIDO co ON t.id_serie = co.id
-WHERE 
-  co.titulo = 'LA LEY Y EL ORDEN UVE';
+-- -- *****************************************************************
+-- SELECT
+--   MIN(t.fecha_estreno) AS Fecha_Primer_Capitulo,
+--   COUNT(DISTINCT t.id) AS Total_Temporadas,
+--   SUM(t.cantidad_capitulos) AS Total_Capitulos,
+--   COUNT(DISTINCT re.id_empresa) AS Empresas_Produccion,
+--   MAX(c.fecha_estreno) AS Fecha_Ultimo_Capitulo
+-- FROM 
+--   TEMPORADA t
+-- JOIN 
+--   CAPITULO c ON t.id = c.id_temporada
+-- JOIN 
+--   R_CONTENIDO_EMPRESA re ON t.id_serie = re.id_contenido
+-- JOIN 
+--   CONTENIDO co ON t.id_serie = co.id
+-- WHERE 
+--   co.titulo = 'LA LEY Y EL ORDEN UVE';
 
 
--- *****************************************************************
-SELECT 
-  p.nombre AS Nombre_Actor,
-  pais_origen.nombre AS Pais_Origen,
-  pais_residencia.nombre AS Pais_Residencia,
-  rca.rol AS Rol,
-  e.fecha AS Ano_Estreno_Pelicula,
-  COUNT(v.id) AS Numero_Visualizaciones
-FROM 
-  CONTENIDO c
-JOIN
-  ESTRENO e on c.id = e.id_contenido
-JOIN 
-  R_CONTENIDO_ACTOR rca ON c.id = rca.id_contenido
-JOIN 
-  PERSONA p ON rca.id_actor = p.id
-JOIN
-  ACTOR a on p.id = a.id_persona
-JOIN 
-  CIUDAD ciudad_origen ON a.id_ciudad_origen = ciudad_origen.id
-JOIN 
-  PAIS pais_origen ON ciudad_origen.id_pais = pais_origen.id
-JOIN 
-  CIUDAD ciudad_residencia ON p.id_ciudad_residencia = ciudad_residencia.id
-JOIN 
-  PAIS pais_residencia ON ciudad_residencia.id_pais = pais_residencia.id
-LEFT JOIN 
-  VISUALIZACION v ON c.id = v.id_contenido
-WHERE 
-  c.titulo = 'AVENGERS: ENDGAME'
-GROUP BY 
-  p.nombre, pais_origen.nombre, pais_residencia.nombre, rca.rol, e.fecha;
+-- -- *****************************************************************
+-- SELECT 
+--   p.nombre AS Nombre_Actor,
+--   pais_origen.nombre AS Pais_Origen,
+--   pais_residencia.nombre AS Pais_Residencia,
+--   rca.rol AS Rol,
+--   e.fecha AS Ano_Estreno_Pelicula,
+--   COUNT(v.id) AS Numero_Visualizaciones
+-- FROM 
+--   CONTENIDO c
+-- JOIN
+--   ESTRENO e on c.id = e.id_contenido
+-- JOIN 
+--   R_CONTENIDO_ACTOR rca ON c.id = rca.id_contenido
+-- JOIN 
+--   PERSONA p ON rca.id_actor = p.id
+-- JOIN
+--   ACTOR a on p.id = a.id_persona
+-- JOIN 
+--   CIUDAD ciudad_origen ON a.id_ciudad_origen = ciudad_origen.id
+-- JOIN 
+--   PAIS pais_origen ON ciudad_origen.id_pais = pais_origen.id
+-- JOIN 
+--   CIUDAD ciudad_residencia ON p.id_ciudad_residencia = ciudad_residencia.id
+-- JOIN 
+--   PAIS pais_residencia ON ciudad_residencia.id_pais = pais_residencia.id
+-- LEFT JOIN 
+--   VISUALIZACION v ON c.id = v.id_contenido
+-- WHERE 
+--   c.titulo = 'AVENGERS: ENDGAME'
+-- GROUP BY 
+--   p.nombre, pais_origen.nombre, pais_residencia.nombre, rca.rol, e.fecha;
 
 
--- *****************************************************************
-SELECT 
-  p.nombre AS Nombre_Usuario,
-  s.fecha_adquisicion AS Fecha_Suscripcion_Antigua,
-  paq.tipo AS Paquete_Asociado
-FROM 
-  PERSONA p
-JOIN 
-  SUSCRIPCION s ON p.id = s.id_cuenta
-JOIN 
-  PAQUETE paq ON s.id_paquete = paq.id
-GROUP BY 
-  p.id
-ORDER BY 
-  COUNT(*) DESC
-LIMIT 1;
+-- -- *****************************************************************
+-- SELECT 
+--   p.nombre AS Nombre_Usuario,
+--   s.fecha_adquisicion AS Fecha_Suscripcion_Antigua,
+--   paq.tipo AS Paquete_Asociado
+-- FROM 
+--   PERSONA p
+-- JOIN 
+--   SUSCRIPCION s ON p.id = s.id_cuenta
+-- JOIN 
+--   PAQUETE paq ON s.id_paquete = paq.id
+-- GROUP BY 
+--   p.id
+-- ORDER BY 
+--   COUNT(*) DESC
+-- LIMIT 1;
